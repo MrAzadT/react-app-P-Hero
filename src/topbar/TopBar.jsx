f1dcfb64e727739c1070b6e536b1df384c3ce97c
azadtamal <mrazadt@gmail.com>
2021-09-19 11:12:41 +0600

    blog

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,7 +3,12 @@ import "./TopBar.css";
 function TopBar() {
   return (
     <div className="topBar">
-      <div className="topLeft">L</div>
+      <div className="topLeft">
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-github-square"></i>
+      </div>
       <div className="topCenter">C</div>
       <div className="topRight">R</div>
     </div>
