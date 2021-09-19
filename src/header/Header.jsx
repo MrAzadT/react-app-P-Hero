a6bae4464f01a8c6425ce409ae99c81e396df3d3
azadtamal <mrazadt@gmail.com>
2021-09-19 22:54:23 +0600

    blog

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header.css";
+import img6 from "../img/img-6.jpg";
 
 function Header() {
   return (
@@ -8,11 +9,7 @@ function Header() {
         <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      <img
-        className="HeaderImg"
-        src="https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-        alt="img"
-      />
+      <img className="HeaderImg" src={img6} alt="img" />
     </div>
   );
 }
