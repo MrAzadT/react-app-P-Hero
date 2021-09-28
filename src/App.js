a5b26f2ac77a3d9a5295bad37993bbd992fee67b
azadtamal <mrazadt@gmail.com>
2021-09-28 23:33:48 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Setting from "./pages/Setting/Setting";
-import Single from "./pages/Single/Single";
+
 import Write from "./pages/Write/Write";
 import TopBar from "./topBar/TopBar";
 
