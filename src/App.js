0f356c36066022966dffbd5b6bcac78724c8952b
azadtamal <mrazadt@gmail.com>
2021-09-28 18:12:25 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import Single from "./pages/Single/Single";
+import Write from "./pages/Write/Write";
 import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <div>
       <TopBar />
-      <Single />
+      <Write />
     </div>
   );
 }
--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -3,7 +3,12 @@ import "./Write.css";
 function Write() {
   return (
     <div className="Write">
-      <h1>Write</h1>
+      <form action="" className="writeForm">
+        <div className="writeFormGroup">
+          <input type="file" id="fileInput" />
+          <input type="text" placeholder="Title" />
+        </div>
+      </form>
     </div>
   );
 }
