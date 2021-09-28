653b14d908e0a276ed2005b35675900e64422709
azadtamal <mrazadt@gmail.com>
2021-09-28 18:24:32 +0600

    blog

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -5,8 +5,11 @@ function Write() {
     <div className="Write">
       <form action="" className="writeForm">
         <div className="writeFormGroup">
-          <input type="file" id="fileInput" />
-          <input type="text" placeholder="Title" />
+          <label htmlFor="fileInput">
+            <i class="fas fa-plus-square"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input type="text" placeholder="Title" className="writeInput" />
         </div>
       </form>
     </div>
