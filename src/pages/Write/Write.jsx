b7127192a4d9ef659c8e0a71b6e899f44b661c3c
azadtamal <mrazadt@gmail.com>
2021-09-28 18:36:20 +0600

    blog

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -9,8 +9,22 @@ function Write() {
             <i class="fas fa-plus-square"></i>
           </label>
           <input type="file" id="fileInput" style={{ display: "none" }} />
-          <input type="text" placeholder="Title" className="writeInput" />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
         </div>
+
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="write your Story...."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit"></button>
       </form>
     </div>
   );
