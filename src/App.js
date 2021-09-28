92f26e0115acb5a2840e721fada9294b9f201928
azadtamal <mrazadt@gmail.com>
2021-09-28 20:59:50 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Setting from "./pages/Setting/Setting";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
@@ -9,7 +10,7 @@ function App() {
   return (
     <div>
       <TopBar />
-      <Login />
+      <Register />
     </div>
   );
 }
